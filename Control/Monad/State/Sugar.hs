5f4bc16d4f9d97ed2d9015503e2a76a4fc128025
Chris Olah <chris@colah.ca>
2012-03-22 14:57:22 -0400

    More flexible assignment operators (=!), (|=) to (=!)

--- Control/Monad/State/Sugar.hs
+++ Control/Monad/State/Sugar.hs
@@ -1,5 +1,4 @@
-{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances, UndecidableInstances, ScopedTypeVariables  #-}
-
+{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances, UndecidableInstances, ScopedTypeVariables, GADTs   #-}
 
 
 module Control.Monad.State.Sugar where
@@ -9,6 +8,10 @@ import Control.Monad.State
 -- We want to be able to store an arbitrarily large
 -- number of monads in our friend state.
 -- We'll store them like so (0,(1,(2,(3,...))))
+
+-- This may seem like it has wasteful boxing.
+-- I take the position: Box them all and let
+-- the compiler sort them out.
 
 -- We need to encode these positions into the 
 -- type system.
@@ -32,15 +35,27 @@ type P7 = NextPos P6
 type P8 = NextPos P7
 type P9 = NextPos P8
 
+-- Also, sometimes we have values derived from expressions. 
+-- Let's have a type for that "position"
+
+-- It is still based on a variable, so we'll place it's position 
+-- as a type inside it.
+
+data ExprPos a = ExprPos a
+
 -- We need to be able to use these to get stuff.
 
 class Accessor position nest result | position nest -> result where
 	accessNest :: position -> nest -> result
 	modifyNest :: position -> (result -> result) -> nest -> nest
 
+-- If BasePos, we access a from (a,b)
+
 instance Accessor BasePos (pres,children) pres where
 	accessNest _ (a,b) = a
 	modifyNest _ mod (a,b) = (mod a, b)
+
+-- If NextPos foo we access whatever is in position foo of b from (a,b)
 
 instance 
 	forall relposition pres children result.
@@ -51,17 +66,46 @@ instance
 	accessNest _ (a,b) = accessNest (undefined :: relposition) b
 	modifyNest _ mod (a,b) = (a, b') where b' = modifyNest (undefined :: relposition) mod b
 
+
+
 -- OK. Now a variable is going position and real type
 
-data Var position actualtype = Var
---                             | VarExpr (State a actualtype)
+data Var position actualtype where
+	Var :: Var position actualtype
+	VarExpr :: 
+		(Var position actualtype) 
+		-> (actualtype -> actualtype2) 
+		-> (Var (ExprPos position) actualtype2)
+
+-- For creating variables. The important part is the type :)
 
 getv :: (Accessor position nest result) => State nest (Var position result)
 getv = return Var
 
+-- We need to be able to evaluate variables/variable expressions. 
+-- For convenience, also non-variables.
 
-class Assignee assignee assigned state | assignee -> assigned  where 
-	assigner :: assignee -> assigned -> State state ()
+class Evaluable start state res where
+	eval :: start -> State state res
+
+instance Evaluable a state a where
+	eval a = return a
+
+-- {-
+instance forall pos res state. (Accessor pos state res) => Evaluable (Var pos res) state res where
+	eval Var = do
+		nest <- get
+		return $ accessNest (undefined :: pos) nest
+-- -}
+
+-- We want to be able assign things other that simple variables.
+-- Basically, pattern matching.
+-- There's a class for that!
+
+class Assignee assignee assigned state | assignee -> assigned where 
+	(=!!) :: assignee -> assigned -> State state ()
+
+-- The basic case of assigning 
 
 instance 
 	forall position state result. 
@@ -69,7 +113,9 @@ instance
 	Assignee (Var position result) result state 
 	where
 
-	assigner _ val = modify (modifyNest (undefined :: position) (\_ -> val))
+	_ =!! val = modify (modifyNest (undefined :: position) (\_ -> val))
+
+-- Assigning two tuples
 
 instance 
 	forall pos1 res1 pos2 res2 state. 
@@ -80,20 +126,38 @@ instance
 	Assignee (Var pos1 res1, Var pos2 res2) (res1, res2) state 
 	where
 
-	assigner _ (val1, val2) = modify $ (modifyNest (undefined :: pos1) (\_ -> val1)) . (modifyNest (undefined :: pos2) (\_ -> val2))
+	(var1, var2) =!! (val1, val2) = do
+		var1 =!! val1
+		var2 =!! val2
+
+-- assigning three tuples
+
+instance 
+	forall pos1 res1 pos2 res2 pos3 res3 state. 
+	(
+		Accessor pos1 state res1,
+		Accessor pos2 state res2,
+		Accessor pos3 state res3
+	) =>
+	Assignee (Var pos1 res1, Var pos2 res2, Var pos3 res3) (res1, res2, res3) state 
+	where
+
+	(var1, var2, var3) =!! (val1, val2, val3) = do
+		var1 =!! val1
+		var2 =!! val2
+		var3 =!! val3
 
 
-(|=) :: 
-	forall position nest result. 
-	(Accessor position nest result) => 
-		Var position result 
-		-> result 
-		-> State nest ()
-var |= val = modify (modifyNest (undefined :: position) (\_ -> val)) 
+var =! preval = do
+	val <- eval preval
+	var =!! val
+
+-- test
 
 a :: State (Int,(Int, children)) ()
 a = do
 	n :: Var P0 Int <- getv
 	m :: Var P1 Int <- getv
-	n |= 2
+	(n,m) =!! (2,3)
+	m =! n
 
--- README.md
+++ README.md
@@ -9,18 +9,37 @@ State.Sugar provides some niceties for working with the State monad. It provides
 An example:
 
 ```haskell
-do 
+test :: State (Int, (String, (Int, ()))) ()
+test = do 
 	-- n is an Int state-variable in position 0
 	n :: Var P0 Int <- getv
 	-- m is an String state-variable in position 1
 	m :: Var P1 String <- getv
 	-- set n = 2, m = "abc"
-	n |= 2
-	m |= "abc"
+	n =!! 2
+	m =!! "abc"
 	-- Then set n = 5, m = "foo" using a nicer notation...
-	(n,m) |= (5,"foo")
+	(n,m) =!! (5,"foo")
 	-- At any point, you can extract the value of a variable into a normal haskell object
-	nval <- val n
+	nval <- eval n
+	-- Which can then be used to do something to change the value of m
+	m =!! (show nval)
+	-- Let's use another variable now...
+	k :: Var P2 Int <- getv
+	-- We can use (=!) to assign variables to variables
+	k =! n
+	-- This works because (=!) evaluates if needed and then assigns.
+	-- It can work in other circumstances, expect the value can't be polymoriphic...
+	-- For example:
+	n =! (1 :: Int) 
+	-- because the typechecker can't infer type type of 1 with (=!)
+	--
+	-- BELOW THIS POINT IS NOT YET IMPLEMENTED!!!!!
+	-- We also provide a while loop!
+	(n,m) = (0,10)
+	while (n < m) $ do
+		n +=!! 1
+		m -=!! 1
 ```
 
 
